a9022ad3560ca4733afa69425c1e9226e62c570f
Verhille <titouan.verhille@epita.fr>
2023-03-16 02:19:32 +0900

    test

--- src/app/api/version/route.ts
+++ src/app/api/version/route.ts
@@ -1,7 +1,9 @@
+import { NextResponse } from "next/server";
+
 export function GET() {
   // return timestamp and and version with variable NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA if it exists
-  return {
+  return NextResponse.json({
     timestamp: new Date().toISOString(),
-    version: process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA || "dev",
-  };
+    version: process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA || "local",
+  });
 }
