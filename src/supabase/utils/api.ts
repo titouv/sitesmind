07fda4f2c079fd712ee189357848f471210a2072
Verhille <titouan.verhille@epita.fr>
2023-03-15 18:49:26 +0900

    try with another client

--- src/supabase/utils/api.ts
+++ src/supabase/utils/api.ts
@@ -1,8 +1,8 @@
 import { Database } from "@/supabase/database.types";
 import { createClient } from "@supabase/supabase-js";
-
+import { createBrowserSupabaseClient } from "@supabase/auth-helpers-nextjs";
 export const createApiClient = () =>
-  createClient<Database>(
-    process.env.NEXT_PUBLIC_SUPABASE_URL,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
-  );
+  createBrowserSupabaseClient<Database>({
+    supabaseUrl: process.env.NEXT_PUBLIC_SUPABASE_URL,
+    supabaseKey: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+  });
