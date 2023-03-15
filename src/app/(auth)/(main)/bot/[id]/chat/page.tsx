588bf5a3c27d5b06115f11b6c0c02e8e87f97bd2
Verhille <titouan.verhille@epita.fr>
2023-03-15 19:31:57 +0900

    remove page, now direct chat component

--- src/app/(auth)/(main)/bot/[id]/chat/page.tsx
+++ src/app/(auth)/(main)/bot/[id]/chat/page.tsx
@@ -1,4 +1,247 @@
-import Chat from "@/app/(auth)/(main)/bot/[id]/chat/chat";
-export default async function Page({ params }: { params: { id: string } }) {
-  return <Chat params={params} />;
+"use client";
+
+import { useState, useRef, useEffect, useMemo } from "react";
+import ReactMarkdown from "react-markdown";
+import remarkGfm from "remark-gfm";
+import {
+  ChatApiSchemaType,
+  OpenAIMessages,
+} from "@/app/(auth)/api/bot/[id]/chat/route";
+import { Textarea } from "@/components/ui/textarea";
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import { Icons } from "@/components/icons";
+import { BASE_URL } from "@/config/site";
+import { Link } from "@/components/ui/link";
+
+type Message = OpenAIMessages[number] & {
+  streaming?: boolean;
+};
+
+export default function Page({ params }: { params: { id: string } }) {
+  const [userInput, setUserInput] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const [messages, setMessages] = useState<Message[]>([
+    {
+      content: "Hello, I am a chatbot. How can I help you?",
+      role: "assistant",
+    },
+  ]);
+
+  // Handle form submission
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+
+    const question = userInput.trim();
+    if (question === "") {
+      return;
+    }
+
+    // clean input
+    setUserInput("");
+    setLoading(true);
+    let messagesToApi: Message[] = [
+      ...messages,
+      {
+        content: question,
+        role: "user",
+      },
+    ];
+    setMessages((state) => [
+      ...state,
+      { content: question, role: "user" },
+      { content: "", role: "assistant", streaming: true },
+    ]);
+    const url = new URL(`api/`, window.location.origin);
+
+    const searchParams: ChatApiSchemaType = {
+      messages: messagesToApi.map((message) => ({
+        content: message.content,
+        role: message.role,
+      })),
+    };
+
+    url.searchParams.set("messages", JSON.stringify(searchParams.messages));
+    url.searchParams.set("botId", params.id);
+
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
+    const data = response.body;
+    if (!data) {
+      return;
+    }
+    const reader = data.getReader();
+    const decoder = new TextDecoder();
+    let done = false;
+    while (!done) {
+      const { value, done: doneReading } = await reader.read();
+
+      if (value) {
+        const decoded = decoder.decode(value);
+        setMessages((state) =>
+          // modify last element
+          state.map((message, index) => {
+            if (index === state.length - 1) {
+              return {
+                ...message,
+                content: message.content + decoded,
+              };
+            }
+            return message;
+          })
+        );
+      }
+      done = doneReading;
+    }
+    // remove streaming
+    setMessages((state) =>
+      state.map((message) => {
+        if (message.streaming) {
+          return {
+            ...message,
+            streaming: undefined,
+          };
+        }
+        return message;
+      })
+    );
+
+    setLoading(false);
+  };
+
+  const messageListRef = useRef<HTMLDivElement>(null);
+  const textAreaRef = useRef<HTMLTextAreaElement>(null);
+
+  const lastMessage = messages[messages.length - 1].content;
+
+  // Auto scroll chat to bottom
+  useEffect(() => {
+    const messageList = messageListRef.current;
+    if (messageList) {
+      messageList.scrollTop = messageList.scrollHeight;
+    }
+  }, [lastMessage]);
+
+  // Focus on text field on load
+  useEffect(() => {
+    textAreaRef.current?.focus();
+  }, [loading]);
+
+  // Prevent blank submissions and allow for multiline input
+  const handleEnter = (e: any) => {
+    if (e.key === "Enter" && userInput) {
+      if (!e.shiftKey && userInput) {
+        handleSubmit(e);
+      }
+    } else if (e.key === "Enter") {
+      e.preventDefault();
+    }
+  };
+
+  return (
+    <div className="container flex flex-col items-center py-8">
+      <div className="flex w-full items-center justify-between py-2  md:w-[75vw]">
+        <h1 className="text-2xl font-bold text-slate-900">Test your chatbot</h1>
+        <Link href={`/bot/${params.id}/how-to`}>
+          How to use the chat on your website
+        </Link>
+      </div>
+      <div className="flex h-[65vh] w-full flex-col items-center justify-center  overflow-hidden rounded-xl border border-slate-300  md:w-[75vw]">
+        <div
+          className="h-full w-full overflow-y-scroll scroll-smooth overflow-x-hidden  scrollbar-thin scrollbar-thumb-slate-400 "
+          ref={messageListRef}
+        >
+          {messages.map((message, index) => {
+            if (message.content == "") return;
+            let icon;
+            let roleClassName;
+
+            if (message.role === "assistant") {
+              icon = (
+                // <Image
+                //   src="/chatIcon.png"
+                //   alt="AI"
+                //   width="30"
+                //   height="30"
+                //   priority
+                // />
+                <div className="rounded-md bg-white p-2">
+                  <Icons.bot
+                    className="h-[14px] w-[14px]"
+                    width={30}
+                    height={30}
+                  />
+                </div>
+              );
+              roleClassName = "bg-slate-100";
+            } else {
+              icon = (
+                //     <Image
+                //       src="/usericon.png"
+                //       alt="Me"
+                //       width="30"
+                //       height="30"
+                //       priority
+                //     />
+                <div className="rounded-md bg-slate-100 p-2">
+                  <Icons.user
+                    className="h-[14px] w-[14px]"
+                    width={30}
+                    height={30}
+                  />
+                </div>
+              );
+
+              // The latest message sent by the user will be animated while waiting for a response
+              roleClassName =
+                index === messages.length - 2 &&
+                messages[messages.length - 1].content == ""
+                  ? "animate-pulse"
+                  : "";
+            }
+            return (
+              <div className={cn("flex px-6 py-4 ", roleClassName)} key={index}>
+                <div className="mr-4">{icon}</div>
+                <div>
+                  <ReactMarkdown
+                    className="prose max-w-none"
+                    remarkPlugins={[remarkGfm]}
+                    linkTarget="_blank"
+                  >
+                    {message.content}
+                  </ReactMarkdown>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+      <div className="w-full pt-4 md:w-[75vw]">
+        <form onSubmit={handleSubmit} className="flex gap-2">
+          <Textarea
+            ref={textAreaRef}
+            disabled={loading}
+            onKeyDown={handleEnter}
+            autoFocus={false}
+            rows={1}
+            maxLength={512}
+            id="userInput"
+            name="userInput"
+            placeholder={
+              loading ? "Waiting for response..." : "Type your question..."
+            }
+            value={userInput}
+            onChange={(e) => setUserInput(e.target.value)}
+            className="w-full resize-none"
+          />
+          <Button type="submit" disabled={loading}>
+            Send
+          </Button>
+        </form>
+      </div>
+    </div>
+  );
 }
